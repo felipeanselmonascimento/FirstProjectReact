3bdfecd4ed8b25688688c005fb1cbe9539d72039
felipe239989 <felipeanselmodonascimento@gmail.com>
2022-11-27 18:53:16 -0300

    configurando themas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -15,7 +15,9 @@ export const ButtonContainer = styled.button<IButtonContainerProps>`
     width: 100px;
     height: 40px;
 
-    ${props => {
+    background-color: ${props => props.theme.primary}
+
+    /* ${props => {
         return `background-color: ${buttonVariants[props.variant]}`
-    }}
+    }} */
 `
