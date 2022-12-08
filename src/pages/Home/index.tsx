99ddd7712a6bfd984b6c7f80bfc65ddfdf9ce274
felipe239989 <felipeanselmodonascimento@gmail.com>
2022-12-07 21:05:04 -0300

    aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,16 +9,29 @@ export function Home() {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">I'm gonna start working in </label>
-                    <TaskInput id="task" placeholder="give a name to your work"/>
+                    <TaskInput id="task"  list="task-suggestions" placeholder="give a name to your work"/>
+
+                    <datalist id="task-suggestions">
+                        <option value="projeto 1"></option>
+                        <option value="projeto 2"></option>
+                        <option value="projeto 3"></option>
+                    </datalist>
 
                     <label htmlFor="minutesAmount">during</label>
-                    <MinutesAmountInput type="number" id="minutesAmount" placeholder="00"/>
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmount"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
 
                     <span>minutes.</span>
                 </FormContainer>
                 <CountdownContainer>
                     <span>0</span>
-                    <span>1</span>
+                    <span>0</span>
                     <Separator>:</Separator>
                     <span>0</span>
                     <span>0</span>
